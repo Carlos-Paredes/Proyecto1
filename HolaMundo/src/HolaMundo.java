a36a76ae1fb9c0808d6f15ce7b9c9cd4f2676523
Carlos-Paredes <carlosparedes130298@gmail.com>
2022-03-30 00:15:33 -0500

    Version proyecto 1

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,14 @@
+//Mi clase en Java
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String args[]){
+       Scanner scanner = new Scanner(System.in);
+        // Escribe tu solucion aqui
+        System.out.println("Proporciona el titulo:");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor:");
+        String autor = scanner.nextLine();
+        System.out.println(titulo + " fue escrito por " + autor);     
+    }
+}
